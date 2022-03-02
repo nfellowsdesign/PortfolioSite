bbaf6d805ffbc07ebe7f944cf940bbb6abed2eeb
Nathan Fellows <nfellowsdesign@gmail.com>
2022-03-02 13:55:44 -0800

    added react router and basic formating

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div>
-      <h1>Portfolio</h1>
+  <div>
+    <div className={styles.hero}>
+      <h1>HERO</h1>
     </div>
+    <div className={styles.section}>
+      <h1>INTRO</h1>
+    </div>
+    <div className={styles.section}>
+      <h1>PROJECTS</h1>
+    </div>
+    <div className={styles.section}>
+      <h1>SKILLS</h1>
+    </div>
+    <div className={styles.section}>
+      <h1>ABOUT</h1>
+    </div>
+    <div className={styles.section}>
+      <h1>CONTACT</h1>
+    </div>
+    <div className={styles.footer}>
+      <h1>✌️</h1>
+    </div>
+  </div>
   );
 }
 
